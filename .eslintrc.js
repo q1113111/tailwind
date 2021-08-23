c6fab7cdfc5b6d3a8c8adc1748dc37c6a5484627
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2021-08-23 12:49:19 +0800

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   // "extends": "plugin:vue/strongly-recommended",
   // "extends": "plugin:vue/recommended" // 最嚴謹
   extends: [
-    'eslint:recommended',
     'plugin:vue/recommended',
     '@vue/standard'
   ],
